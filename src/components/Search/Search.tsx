099e33d8b104e99f2e9a97c9f0fad350c04447d1
Franco Palumbo <palumbofranco9a@gmail.com>
2021-05-22 12:51:36 -0300

    Search bar handle function

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -9,18 +9,27 @@ const Search = () => {
         console.log(id)
     }
 
-    const actionList = actions.map((item) =>
+    function emptyActions(){
+
+    }
+
+    let actionList = actions.map((item) =>
         <div className="action-card" key={item.id} onClick={() => viewDetails(item.id)}>
             <h2 className="action-card-title">{item.action_name} ({item.symbol})</h2>
             <p className="action-card-company">{item.company_name}</p>
             <p className="action-card-price">${item.price}</p>
         </div>
     )
+
+
     return (
         <div>
             <SearchBar/>
             <div className="container">
                 {actionList}
+            </div>
+            <div className="search-unknown" hidden={true}>
+                Unable to find the action you were looking for
             </div>
         </div>
     );
--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,11 +1,24 @@
 import "./SearchBar.css";
 
+function handleSearch(){
+    console.log("Do smth")
+}
+
 const SearchBar = () => (
+    /*<div>
+        <form onSubmit={handleSearch}>
+
+        </form>
+    </div>*/
     <div className="component-search-input">
         <input
             type="text"
             id="action-search"
-            placeholder="Search Action"/>
+            placeholder="Search Action"
+            maxLength= {30}
+            minLength= {2}
+            onChange={handleSearch}
+        />
     </div>
 );
 
