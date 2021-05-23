8c9cd40bde6e51e8bdfde83a5b9425e491feb234
Alejo Ramirez Gismondi <ramirezgismondialejo@gmail.com>
2021-05-23 15:34:55 -0300

    click test

--- src/components/ActionDetails/ActionDetails.tsx
+++ src/components/ActionDetails/ActionDetails.tsx
@@ -4,6 +4,10 @@ const ActionDetails = () => {
   return (
     <div>
       <h1 data-testid={"detail-id"}>Hello World</h1>
+      <button data-testid={"detail-button-id"} onClick={() => {
+        console.log("Clicked")
+      }}>Click me!
+      </button>
     </div>
   );
 }
--- src/components/ActionDetails/__test__/ActionDetails.test.tsx
+++ src/components/ActionDetails/__test__/ActionDetails.test.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ActionDetails from "../ActionDetails";
 import {cleanup, render} from "@testing-library/react";
 import renderer from 'react-test-renderer';
+import { fireEvent } from '@testing-library/react';
 
 afterEach(cleanup);
 
@@ -16,7 +17,26 @@ it('should render correctly', function () {
   expect(getByTestId("detail-id")).toHaveTextContent("Hello World")
 });
 
+it('should console log something when button is clicked', function () {
+  const { getByTestId } = render(<ActionDetails/>);
+  const consoleSpy = jest.spyOn(console, 'log');
+  fireEvent.click(getByTestId("detail-button-id"));
+  expect(consoleSpy).toHaveBeenCalledWith('Clicked');
+});
+
 it('should match snapshot', function () {
   const tree = renderer.create(<ActionDetails/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+// it('CheckboxWithLabel changes the text after click', () => {
+//   const {queryByLabelText, getByLabelText} = render(
+//     <ActionDetails labelOn="On" labelOff="Off" />,
+//   );
+//
+//   expect(queryByLabelText(/off/i)).toBeTruthy();
+//
+//   fireEvent.click(getByLabelText(/off/i));
+//
+//   expect(queryByLabelText(/on/i)).toBeTruthy();
+// });
