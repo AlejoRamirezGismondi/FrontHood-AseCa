3431f32c42eb3f795262f4660608748ba4bf5281
Franco Palumbo <palumbofranco9a@gmail.com>
2021-06-01 10:24:41 -0300

    Search bar input value test

--- src/components/Search/_test_/Search.test.tsx
+++ src/components/Search/_test_/Search.test.tsx
@@ -6,35 +6,71 @@ import { fireEvent } from '@testing-library/react';
 import Search from "../Search";
 import {BrowserRouter} from 'react-router-dom';
 import SearchBar from "../SearchBar";
+import {getByTestId} from "@testing-library/react";
 
-afterEach(cleanup);
+let root;
 
-it("renders without crashing", () => {
+beforeAll(() => {
     const div = document.createElement("div");
     ReactDOM.render(
         <BrowserRouter><Search/></BrowserRouter>, div);
+    root = div;
 })
 
-it('should render input with place holder Search Action', function () {
-    const { getByTestId } = render(<BrowserRouter><Search/></BrowserRouter>);
-    expect(getByTestId("search-input-id")).toHaveProperty("placeholder", "Search Action")
-});
+/*it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <BrowserRouter><Search/></BrowserRouter>, div);
+})*/
 
-it('should render input with max length 30 and min length 2', function () {
-    const { getByTestId } = render(<SearchBar/>);
-    const input = getByTestId("search-input-id")
-    expect(input).toHaveProperty("maxLength", 30)
-    expect(input).toHaveProperty("minLength", 2)
-});
+describe('SearchBar setup', () => {
+    it('should render input with place holder Search Action', function () {
+        expect(getByTestId(root, "search-input-id")).toHaveProperty("placeholder", "Search Action")
+    });
+
+    it('should render input with empty value', function () {
+        expect(getByTestId(root, "search-input-id")).toHaveProperty("value", "")
+    });
+
+    it('should render input with max length 30 and min length 2', function () {
+        const { getByTestId } = render(<SearchBar/>);
+        const input = getByTestId("search-input-id")
+        expect(input).toHaveProperty("maxLength", 30)
+        expect(input).toHaveProperty("minLength", 2)
+    });
+})
+
+describe('Search setup', () => {
+    it('should match snapshot', function () {
+        //run npm test
+        const tree = renderer.create(<BrowserRouter><Search/></BrowserRouter>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('there should be actions in the menu', function () {
+        const { getByTestId } = render(<BrowserRouter><Search/></BrowserRouter>);
+        const container = getByTestId("search-container-id")
+        expect(container.childElementCount).toBeGreaterThanOrEqual(1)
+    });
+})
 
 
 
-/*it('', function () {
-    const component = render(<BrowserRouter><Search/></BrowserRouter>)
-    //why undefine?
-    //console.log(component.state)
-    //expect(component.state.searchInput).toBe('')
-});*/
+describe('Input value change', () =>{
+    it('should change the value of the input', () => {
+        const input = getByTestId(root,"search-input-id")
+        fireEvent.change(input, { target: { value: 'ADBE' } })
+        // @ts-ignore
+        expect(input.value).toBe('ADBE')
+        // @ts-ignore
+        expect(input.value).not.toBe('TESLA')
+    })
+
+    /*it('should', () => {
+        expect(root.state.searchInput).toBe("")
+    })*/
+})
+
 
 
 
@@ -56,19 +92,11 @@ it('should console log something when search button is clicked', function () {
     expect(consoleSpy).toHaveBeenCalledWith("Fake Search Stocks");
 });
 
-it('there should be actions in the menu', function () {
-    const { getByTestId } = render(<BrowserRouter><Search/></BrowserRouter>);
-    const container = getByTestId("search-container-id")
-    expect(container.childElementCount).toBeGreaterThanOrEqual(1)
-});
 
 
 
-it('should match snapshot', function () {
-    //run npm test
-    const tree = renderer.create(<BrowserRouter><Search/></BrowserRouter>).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+
+
 
 
 it('action card should have Name, Code, Company and Score', () => {
